75773da793fadc865a234b39d725112f7365cbe5
Alex C <59523055+Alexc09@users.noreply.github.com>
2021-04-26 18:27:18 +0800

    Testing stuff

---
+++ routes/market.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const router = express.Router()
+
+router.get('/', (req, res)=>{
+    const metadata = {
+        title: "YESYESYES"
+    }
+    res.render("customer-home", metadata)
+})
+
+
+module.exports = router
