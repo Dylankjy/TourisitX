d8c43c607d5434038e569edef1e4a465f820acce
Dylan Kok <tenkotofu@gmail.com>
2021-05-02 14:52:42 +0800

    Added slideshow for hero

---
+++ public/js/slideshow.js
@@ -0,0 +1,11 @@
+// Slideshow for hero
+var hero = 1
+setInterval(() => {
+  $("section.hero#main-hero").removeClass("preload-" + hero)
+  hero++
+  $("section.hero#main-hero").addClass("preload-" + hero)
+  if (hero > 10) {
+    $("section.hero#main-hero").removeClass("preload-" + hero)
+    hero = 0
+  }
+}, 5000);
