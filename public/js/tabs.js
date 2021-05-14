9ab5521b18671e5b63dde6fcd55b00861d1782bd
chloryfish <72703739+chloryfish@users.noreply.github.com>
2021-05-14 08:16:12 +0800

    Update tab.js

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,13 +1,11 @@
-function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("content-tab");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+openTab = (evt, tabName) => {
+    let i; let x; let tablinks
+    for (i = 0; i < $('.content-tab').length; i++) {
+        $('.content-tab')[i].style.display = 'none'
     }
-    tablinks = document.getElementsByClassName("tab");
-    for (i = 0; i < x.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    for (i = 0; i < $('.content-tab').length; i++) {
+        $('.tab')[i].className = $('.tab')[i].className.replace(' is-active', '')
     }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " is-active";
+    document.getElementById(tabName).style.display = 'block'
+    evt.currentTarget.className += ' is-active'
 }
