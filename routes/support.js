560d793a5ed3b2759750b09be7819dca31ed64b7
Dylan Kok <tenkotofu@gmail.com>
2021-05-14 12:20:37 +0800

    Format support.js

--- routes/support.js
+++ routes/support.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 // router.get('/', (req, res) => { ... }
 router.get('/helpdesk', (req, res) => {
-     res.render('users/profile.hbs')
+    res.render('users/profile.hbs')
 })
 
 module.exports = router
