9a91d296f54a9bdb5e85656489e51a51b7f7a3a2
Dylan Kok <tenkotofu@gmail.com>
2021-05-15 17:45:32 +0800

    Add list tour guide payments UI and routing

--- routes/tourguide.js
+++ routes/tourguide.js
@@ -4,6 +4,24 @@ const router = express.Router()
 
 // Put all your routings below this line -----
 
+const exampleTransaction = {
+    date_paid: new Date('2011-10-05T14:48:00.000Z'),
+    tour_name: 'Sex on the beach',
+    cust_id: 'Takahashi Taro',
+    tg_id: 'Nakamoto Yui',
+    earnings: '1000',
+    status: true,
+}
+
+const exampleTransaction2 = {
+    date_paid: new Date('2011-10-05T14:48:00.000Z'),
+    tour_name: 'City Dwelling',
+    cust_id: 'Ri Ui',
+    tg_id: 'Nakamoto Yui',
+    earnings: '1000',
+    status: false,
+}
+
 // router.get('/', (req, res) => { ... }
 router.get('/', (req, res) => {
     const metadata = {
@@ -94,6 +112,23 @@ router.get('/bookings', (req, res) => {
         layout: 'tourguide',
     }
     res.render('tourguide/dashboard/bookings', metadata)
+})
+
+router.get('/payments', (req, res) => {
+    const metadata = {
+        meta: {
+            title: 'Payments',
+            path: false,
+        },
+        nav: {
+            sidebarActive: 'payments',
+        },
+        layout: 'tourguide',
+        data: {
+            transactions: { exampleTransaction, exampleTransaction2 },
+        },
+    }
+    res.render('tourguide/dashboard/payments', metadata)
 })
 
 module.exports = router
