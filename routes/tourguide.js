9ceb0c1c4b70743bbb34a400abdb9f9e8866f502
Dylan Kok <tenkotofu@gmail.com>
2021-05-15 17:44:28 +0800

    Add dashboard UI and routing

--- routes/tourguide.js
+++ routes/tourguide.js
@@ -5,5 +5,18 @@ const router = express.Router()
 // Put all your routings below this line -----
 
 // router.get('/', (req, res) => { ... }
+router.get('/', (req, res) => {
+    const metadata = {
+        meta: {
+            title: 'Your Desk',
+            path: false,
+        },
+        nav: {
+            sidebarActive: 'desk',
+        },
+        layout: 'tourguide',
+    }
+    res.render('tourguide/dashboard/dashboard', metadata)
+})
 
 module.exports = router
