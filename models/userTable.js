4ca20682412825431c316c55a5820a35a0a209b8
CoolMysticIon <dragoamy@gmail.com>
2021-06-19 13:09:57 +0800

    Create userTable.js

---
+++ models/userTable.js
@@ -0,0 +1,102 @@
+module.exports = (sequelize, DataTypes) => {
+     const User = sequelize.define('User', {
+
+          id: {
+               allowNull: false,
+               primaryKey: true,
+               type: DataTypes.UUID,
+          },
+
+          name: {
+               allowNull: false,
+               type: DataTypes.STRING,
+          },
+
+          password: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          email: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          phone_number: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          bio: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          profile_img: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          registration_time: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          stripe_id: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          wishlist: {
+               type: DataTypes.ARRAY,
+               allowNull: false,
+          },
+
+          fb: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          insta: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          li: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          email_status: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+          
+          phone_status: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          account_type: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          verified: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+          ip_address: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+
+     },
+
+          {
+               tableName: 'User',
+          })
+
+     return User
+}
