ad1740b4db4fdc2bb8c8f2161c072b81205ae29c
Dylan Kok <tenkotofu@gmail.com>
2021-06-24 16:35:57 +0800

    Added API functions

---
+++ app/genkan/genkan.js
@@ -0,0 +1,28 @@
+// This file mostly contains methods to use in other to get certain variables out.
+// I made this because we are all lazy people and no one gives 10 flying shits about SQLize.
+// -- Dylan
+
+// Database operations
+require('../db')
+
+const getUserByID = (uid, callback) => {
+    findDB('user', { 'id': uid }, (userResult) => {
+        return callback(userResult[0].dataValues)
+    })
+}
+
+
+const getUserBySession = (sid, callback) => {
+    findDB('session', { 'sessionId': sid }, (sessionResult) => {
+        if (result.length !== 1) {
+            return callback(null)
+        }
+
+        getUserByID(sessionResult[0].dataValues.userId, (userResult) => {
+            return callback(userResult)
+        })
+    })
+}
+
+module.exports = getUserByID
+module.exports = getUserBySession
