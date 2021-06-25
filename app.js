2fa207ba5743b9b4d019f0ae142f264bc436f9c2
Dylan Kok <tenkotofu@gmail.com>
2021-06-26 04:11:48 +0800

    Remove unused cookieParser code in app.js
    Each file should have it's own import and not a universal one to prevent conflicting libraries accessing the POST method.

--- app.js
+++ app.js
@@ -82,9 +82,6 @@ app.engine('hbs', exphbs({
 // Handlebars: Views folder
 app.set('views', [`views`])
 
-// cookieParser: Secret key for signing
-app.use(cookieParser('Please change this when in production use'))
-
 // Formidable: For POST data accessing
 // THIS IS DISABLED AS IT INTERFERES WITH POST PROCESSING FOR GENKAN
 // IF YOU REQUIRE THIS MODULE, PLEASE INCLUDE IT INSIDE YOUR ROUTING FILES
