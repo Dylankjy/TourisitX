50bab5a652384c652937b6f13a83af2f3a658fb5
CoolMysticIon <dragoamy@gmail.com>
2021-06-28 22:57:42 +0800

    Settings auth

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const { static } = require('express')
 const express = require('express')
+const genkan = require('app/genkan/genkan')
+
 
 const router = express.Router()
 
@@ -33,7 +35,9 @@ router.get('/profile', (req, res) => {
     })
 })
 
-router.get('/setting/general', (req, res) => {
+router.get('/setting/general',async (req, res) => {
+    var sid = req.signedCookies.sid
+    var user = await genkan.getUserBySessionAsync(sid)
     const metadata = {
         meta: {
             title: 'General Setting',
@@ -43,19 +47,7 @@ router.get('/setting/general', (req, res) => {
             sidebarActive: 'general',
         },
         layout: 'setting',
-        user: {
-            name: 'Harold Chan',
-            password: 'password',
-            email: 'test@email.com',
-            bio: 'Hide The Pain',
-            pfp: '/static/pfp.jpg',
-            phone_number: '12348765',
-            account_mode: 0,
-            fb: 'https://www.facebook.com/',
-            insta: 'https://www.instagram.com/',
-            li: 'https://www.linkedin.com/company/paul-immigrations/',
-
-        },
+        user,
     }
     return res.render('users/general.hbs', metadata)
 })
