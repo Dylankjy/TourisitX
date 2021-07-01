63cf53c2dec9fdd02ec88e59b6bc528ef584d0b1
Dylan Kok <tenkotofu@gmail.com>
2021-07-02 02:21:34 +0800

    Added 'create' functions for room and messages
    This is untested. Please ensure that you test this before merging into develop

--- app/chat/chat.js
+++ app/chat/chat.js
@@ -5,10 +5,56 @@
 // Database operations
 require('../db')
 
-addMessage = () => {
+// UUID
+const uuid = require('uuid')
+
+addRoom = (participants, bookingId, callback) => {
+    if (typeof (participants) !== 'object') {
+        throw new Error('Participants must of type object in a valid format.')
+    }
+
+    const AddRoomPayload = {
+        'chatId': uuid.v1(),
+        'participants': participants,
+        'bookingId': bookingId,
+    }
+
+    insertDB('chat', AddRoomPayload, (result) => {
+        // In rare situations, should the db operation fail, do not panic.
+        // It's okay if everything decides to burn up and perish.
+        if (result !== true) {
+            return callback(false)
+        }
+
+        // Returning callback will contain the UUID for that chat.
+        return callback(AddRoomPayload.chatId)
+    })
 }
 
+addMessage = (roomId, senderId, messageText, flag, callback) => {
+    if (senderId === 'SYSTEM') {
+        senderId = '00000000-0000-0000-0000-000000000000'
+    }
 
-module.exports = {
-    undefined,
+    const AddMessagePayload = {
+        'roomId': roomId,
+        'senderId': senderId,
+        'messageText': messageText,
+        'flag': flag,
+    }
+
+    insertDB('chatMessages', AddMessagePayload, () => {
+        return callback(true)
+    })
 }
+
+const exports = {
+    'room': {
+        add: addRoom,
+    },
+    'msg': {
+        add: addMessage,
+    },
+}
+
+module.exports = exports
