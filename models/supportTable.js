0ec3981acce05a5cb57f8a5751026cf4bcc5c44a
CoolMysticIon <dragoamy@gmail.com>
2021-07-01 11:02:24 +0800

    es linting

--- models/supportTable.js
+++ models/supportTable.js
@@ -1,55 +1,55 @@
 const { Model } = require('sequelize')
 
 module.exports = (sequelize, DataTypes) => {
-     class Support extends Model {
-          //
-     }
+    class Support extends Model {
+        //
+    }
 
-     Support.init(
-          {
-               ticket_id: {
-                    allowNull: false,
-                    primaryKey: true,
-                    type: DataTypes.UUID,
-               },
+    Support.init(
+        {
+            ticket_id: {
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+            },
 
-               uid: {
-                    allowNull: false,
-                    type: DataTypes.UUID,
-               },
+            uid: {
+                allowNull: false,
+                type: DataTypes.UUID,
+            },
 
-               u_email: {
-                    type: DataTypes.STRING(64),
-                    allowNull: false,
-               },
+            u_email: {
+                type: DataTypes.STRING(64),
+                allowNull: false,
+            },
 
-               support_type: {
-                    type: DataTypes.STRING(32),
-                    allowNull: false,
-               },
+            support_type: {
+                type: DataTypes.STRING(32),
+                allowNull: false,
+            },
 
-               content: {
-                    type: DataTypes.STRING(64),
-                    allowNull: false,
-               },
+            content: {
+                type: DataTypes.STRING(64),
+                allowNull: false,
+            },
 
-               link: {
-                    type: DataTypes.STRING(64),
-                    allowNull: true,
-               },
+            link: {
+                type: DataTypes.STRING(64),
+                allowNull: true,
+            },
 
-               status: {
-                    type: DataTypes.STRING(64),
-                    allowNull: false,
-               },
-          },
+            status: {
+                type: DataTypes.STRING(64),
+                allowNull: false,
+            },
+        },
 
-          { /* hon hon french bread */
-               sequelize,
-               tableName: 'Support',
-               modelName: 'Support',
-          },
-     )
+        { /* hon hon french bread */
+            sequelize,
+            tableName: 'Support',
+            modelName: 'Support',
+        },
+    )
 
-     return Support
+    return Support
 }
