42d8aeb0d84c1282b265dffd3f652e1d544dd331
Dylan Kok <tenkotofu@gmail.com>
2021-07-26 16:30:10 +0800

    Improve admin API functions

--- routes/api.js
+++ routes/api.js
@@ -9,33 +9,24 @@ const { adminAuthorisationRequired, loginRequired } = require('../app/genkan/mid
 const { Booking, User } = require('../models')
 const { Op } = require('sequelize')
 
+// Admin Fetch API
+const { getAdminStats, getUserStats } = require('../app/api/admin')
+
 // Admin Panel API
 router.get('/admin', adminAuthorisationRequired, async (req, res) => {
-    // Calculating moneys
-    const wildcardBookingRecords = await Booking.findAll({})
-    const totalIncome = wildcardBookingRecords.map((booking) => parseFloat(booking.bookBaseprice) + parseFloat(booking.bookCharges)).reduce((a, b) => a + b)
-    const totalBookings = wildcardBookingRecords.length
-    const totalGrossProfits = totalIncome * 0.15
-
-    // User analytics
-    const totalUsers = (await User.findAll({})).length
-    const totalActiveUsers = (await User.findAll({ createdAt: { [Op.between]: [(new Date()) - 24, new Date()] } })).length
-    const totalNewUsers = (await User.findAll({ createdAt: { [Op.between]: [(new Date()) - 4, new Date()] } })).length
-
     // Custom Satisfication
     // TODO: Implement, wait for table to be created.
 
     // Final JSON object to be returned
     const ResponseObject = {
         money: {
-            totalIncome,
-            totalBookings,
-            totalGrossProfits,
+            currentPeriod: await getAdminStats(30),
+            lastPeriod: await getAdminStats(60),
         },
+        // totalNetIncomeDiff,
         users: {
-            totalUsers,
-            totalActiveUsers,
-            totalNewUsers,
+            currentPeriod: await getUserStats(30),
+            lastPeriod: await getUserStats(60),
         },
         csat: {
             'Work in Progress': 1,
@@ -44,5 +35,9 @@ router.get('/admin', adminAuthorisationRequired, async (req, res) => {
 
     return res.json(ResponseObject)
 })
+
+// router.get('/admin/charts', adminAuthorisationRequired, () => {
+
+// })
 
 module.exports = router
