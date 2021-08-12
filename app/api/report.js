ecefd3793b969cd43f37ed99a1025eb84d820b2b
Dylan Kok <tenkotofu@gmail.com>
2021-08-12 15:59:38 +0800

    Delete report.js
    Unused file

--- app/api/report.js
+++
@@ -1,7 +0,0 @@
-const { Booking, User } = require('../../models')
-const { Op } = require('sequelize')
-
-// Formula to get number of ms in a day -- This is here because I don't want to have to keep typing this.
-const oneDay = 24 * 60 * 60 * 1000
-
-// const a = () => { }
