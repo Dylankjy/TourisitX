74d88ee2761961e90dd3b4fe40072c484806e220
BuilderAmy <dragoamy@gmail.com>
2021-08-13 13:24:17 +0800

    working star rating

--- routes/booking.js
+++ routes/booking.js
@@ -71,12 +71,16 @@ router.get('/', async (req, res) => {
             attributes: ['bookId', 'processStep', 'completed'],
             // order: [['updatedAt', 'DESC']],
             include: [
-                { model: Shop,
-                    attributes: ['tourTitle', 'tourImage'] },
-                { model: Review,
+                {
+                    model: Shop,
+                    attributes: ['tourTitle', 'tourImage'],
+                },
+                {
+                    model: Review,
                     required: false,
                     attributes: ['id'],
-                    where: { reviewerId: req.currentUser.id } }],
+                    where: { reviewerId: req.currentUser.id },
+                }],
             raw: true,
             limit: pageSize,
             offset: offset,
@@ -100,12 +104,16 @@ router.get('/', async (req, res) => {
             attributes: ['bookId', 'processStep'],
             order: [['updatedAt', 'ASC']],
             include: [
-                { model: Shop,
-                    attributes: ['tourTitle'] },
-                { model: Review,
+                {
+                    model: Shop,
+                    attributes: ['tourTitle'],
+                },
+                {
+                    model: Review,
                     required: false,
                     where: sequelize.literal('Reviews.type != "CUST"'),
-                    attributes: ['id', 'reviewerId', 'type', 'reviewText'] }],
+                    attributes: ['id', 'reviewerId', 'type', 'reviewText'],
+                }],
             raw: true,
         })
         console.log(reqAction)
@@ -119,8 +127,10 @@ router.get('/', async (req, res) => {
             attributes: ['bookId', 'processStep', 'tourStart'],
             order: [['tourStart', 'DESC']],
             include:
-                    { model: Shop,
-                        attributes: ['tourTitle'] },
+            {
+                model: Shop,
+                attributes: ['tourTitle'],
+            },
             raw: true,
         })
     }
@@ -194,7 +204,7 @@ router.post('/:id/request-revision', async (req, res) => {
             custRequests: newReq,
         }, {
             where: { bookId: bookId },
-        }).then(() =>{
+        }).then(() => {
             TourPlans.update({
                 accepted: '-1',
             }, {
@@ -236,7 +246,7 @@ router.post('/:id/accept-plan', async (req, res) => {
         processStep: '3',
     }, {
         where: { bookId: bookId },
-    }).then(() =>{
+    }).then(() => {
         TourPlans.update({
             accepted: '1',
         }, {
@@ -297,7 +307,7 @@ router.post('/:id/complete-tour', async (req, res) => {
         completed: 1,
     }, {
         where: { bookId: bookId },
-    }).then(() =>{
+    }).then(() => {
         addMessage(bookData['chatId'], 'SYSTEM', '<customer> declared that the tour is complete.', 'ACTIVITY', () => {
             res.redirect(`/bookings/${bookId}`)
         })
@@ -306,7 +316,7 @@ router.post('/:id/complete-tour', async (req, res) => {
 
 router.post('/:id/review-tour', async (req, res) => {
     const bookId = req.params.id
-    console.log(req.fields)
+    console.table(req.fields)
 
     const v = new Validator(req.fields)
     const ratingResult = v
